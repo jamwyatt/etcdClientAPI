0bcc58574ef1d69814b9e07f9983bff34070a895
J. Robert Wyatt <jamwyatt@gmail.com>
2015-10-25 12:17:16 -0700

    Added cancelation channel to control blocking read

--- etcdMisc/Watcher.go
+++ etcdMisc/Watcher.go
@@ -2,15 +2,21 @@ package etcdMisc
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
 )
 
-func Watcher(key string, recursive bool, waitIndex ...int) (WatchResponse, error) {
+func Watcher(ctrl chan bool, key string, recursive bool, waitIndex ...int) (WatchResponse, error) {
+	var err error
+	tr := &http.Transport{
+		DisableKeepAlives:     true, // No persistent connections
+		ResponseHeaderTimeout: 0,    // No timeouts
+	}
 	client := &http.Client{
-		Timeout: 0, // No timeout for watch
+		Timeout:   0, // No timeout for watch
+		Transport: tr,
 	}
 
 	url := fmt.Sprintf("http://localhost:4001/v2/keys/%s?wait=true&recursive=%t", key, recursive)
@@ -18,26 +24,42 @@ func Watcher(key string, recursive bool, waitIndex ...int) (WatchResponse, error
 		url += fmt.Sprintf("&waitIndex=%d", waitIndex[0])
 	}
 	fmt.Println("URL: ", url)
-
-	resp, err := client.Get(url)
+	var request *http.Request
+	request, err = http.NewRequest("GET", url, nil)
 	if err != nil {
-		fmt.Println("GET Error:", err)
-		os.Exit(-1)
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Body Error:", err)
-		os.Exit(-1)
-	}
-	// fmt.Printf("body: %v\n", string(body))
-
-	r := WatchResponse{}
-	err = json.Unmarshal(body, &r)
-	if err != nil {
-		fmt.Println("Failed to decode response")
+		fmt.Println("Request Error:", err)
 		return WatchResponse{}, err
 	}
-	return r, nil
+
+	syncChannel := make(chan WatchResponse)
+	go func(s chan WatchResponse) {
+		var resp *http.Response
+		resp, err = client.Do(request)
+		if err != nil {
+			fmt.Println("GET Error:", err)
+			s <- WatchResponse{}
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Body Error:", err)
+			s <- WatchResponse{}
+		}
+		r := WatchResponse{}
+		err = json.Unmarshal(body, &r)
+		if err != nil {
+			fmt.Println("Failed to decode response")
+			s <- WatchResponse{}
+		}
+		s <- r
+	}(syncChannel)
+
+	select {
+	case msg := <-syncChannel:
+		return msg, err
+	case <-ctrl:
+		tr.CancelRequest(request)
+		return WatchResponse{}, errors.New("Canceled")
+	}
+
 }
--- etcdWatcherTest/main.go
+++ etcdWatcherTest/main.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"github.com/jamwyatt/etcdClientAPI/etcdMisc"
 	"os"
+	"time"
 )
+
+// Test will watch the target twice and then timeout the last
+// Drive the test by updating the target in etcd twice and then wait for the timeout.
 
 func main() {
 	if len(os.Args) < 1 {
@@ -12,8 +16,10 @@ func main() {
 		os.Exit(-1)
 	}
 
+	ctrl := make(chan bool)
+
 	// Watch for the next event
-	r, err := etcdMisc.Watcher(os.Args[1], true)
+	r, err := etcdMisc.Watcher(ctrl, os.Args[1], true)
 	if err != nil {
 		fmt.Println("Failed")
 	} else {
@@ -21,11 +27,24 @@ func main() {
 	}
 
 	// Watch for the 'proper' next event, by number
-	r, err = etcdMisc.Watcher(os.Args[1], true, r.Node.ModifiedIndex+1)
+	r, err = etcdMisc.Watcher(ctrl, os.Args[1], true, r.Node.ModifiedIndex+1)
 	if err != nil {
 		fmt.Println("Failed")
 	} else {
 		fmt.Printf("WatchResponse: %s\n", r)
 	}
+
+	// Watch for an event ad then cancel it.
+	go func() {
+		r, err := etcdMisc.Watcher(ctrl, os.Args[1], true)
+		if err != nil {
+			fmt.Println("Passed with expected fail", err)
+		} else {
+			fmt.Printf("Failed with WatchResponse: %s\n", r)
+		}
+	}()
+	time.Sleep(time.Second * 2)
+	ctrl <- true
+	time.Sleep(time.Second * 1)
 
 }
